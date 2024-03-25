528d532551bf9e17bdbf156a642ccdec6db9e0f0
ChristinaYU333 <157336100+ChristinaYU333@users.noreply.github.com>
2024-03-24 22:02:37 -0400

    Add files via upload

---
+++ transformation/js/script.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Am I Perfect?</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+</head>
+<body>
+  <h1>Am I Perfect?</h1>
+  <button id="perfectButton">Click to Find Out</button>
+  <p id="answer" style="display: none;">Yes, you are absolutely perfect!</p>
+  <script src="js/script.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
